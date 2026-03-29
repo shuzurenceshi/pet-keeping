9712fa91db7f7236cbce969605f78cc2621926ca
root <root@localhost.localdomain>
2026-03-29 23:54:20 +0800

    auto sync: 2026-03-29 23:54:20

--- api/server.js
+++
@@ -1,6 +0,0 @@
-// Vercel Serverless Function
-export default async function handler(req, res) {
-  // 动态导入后端服务
-  const { default: app } = await import('../backend/src/server.js')
-  return app.fetch(req, res)
-}
